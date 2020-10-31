6907d01912546a2a29c4054d93386db7f913d07c
dipsscor <dipsscor@gmail.com>
2020-10-31 12:08:41 +0530

    changes

--- customer-service/src/main/java/com/dipsscor/customerservice/WebFluxConfig.java
+++ customer-service/src/main/java/com/dipsscor/customerservice/WebFluxConfig.java
@@ -1,8 +1,9 @@
 package com.dipsscor.customerservice;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
-
+@Configuration
 @EnableWebFlux
 public class WebFluxConfig implements WebFluxConfigurer{
 
--- gateway-service/src/main/java/com/dipsscor/gatewayservice/WebFluxConfig.java
+++ gateway-service/src/main/java/com/dipsscor/gatewayservice/WebFluxConfig.java
@@ -1,9 +1,11 @@
 package com.dipsscor.gatewayservice;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @EnableWebFlux
+@Configuration
 public class WebFluxConfig implements WebFluxConfigurer{
 
 }
